c95c37c9a514665304865e1a7d2600b50197e9f5
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-11-27 02:24:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
-const badges = ['Lisa', 'Kaitlin', 'Jan'];
+function printBadges(arrayNames){
+  ['Lisa', 'Kaitlin', 'Jan'];
 
-function printBadges(badges){
   for (let i = 0; i < badges.length; i++){
     console.log(`Welcome ${badges[i]}! You are employee #[badges.length]`)
   }
