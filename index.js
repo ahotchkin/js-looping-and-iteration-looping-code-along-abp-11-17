67246f07731d939380ce75f01f4de6fabb330b39
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-11-27 02:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function printBadges(arrayNames){
 
   return arrayNames;
 }
+
+function coinToss(){
+  return Math.random() => 0.5;
+}
+
+function tailsNeverFails(){
+  let counter = 0;
+  while (coinToss()){
+    counter++
+  }
+
+  return `You got ${counter} tails in a row.`
+
+}
